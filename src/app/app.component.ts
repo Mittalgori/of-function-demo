1761d1beffe47ab4c6c5b35999b01e57358f685f
Mittalgori <mitu796@gmail.com>
2024-05-08 10:13:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'of-function-demo';
+  emittedValuesFromObservable: string[] = []
+
+  ngOnInit(): void {
+    const myObservable$ = of('Apple', 'Banana', 'Kiwi', 'Orane', 'Mango');
+
+    myObservable$.subscribe(
+      (data) => {
+        console.log('REceived data value : ', data);
+        this.emittedValuesFromObservable.push(data);
+      },
+      (error) => {
+        console.log('Error : ', error)
+      },
+      () => {
+        console.log("Observable complete")
+      }
+    );
+  }
 }
